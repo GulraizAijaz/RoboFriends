d366713eaa500bac8d30ad85416ca0111c0abce3
GulraizAijaz <you@example.com>
2024-03-18 15:25:29 +0500

    initial commit

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Scroll = (props) => {
+    return( 
+     <div className="scrollcomponent" > 
+      {props.children}
+      </div>
+      
+  )
+  };
+
+  export default Scroll
---
+++ src/containers/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+import Searchbox from "../components/Searchbox"
+import './App.css'
+import CardList from "../components/CardList";
+import Scroll from "../components/Scroll";
+
+function App() {
+  const [robots,setRobots] = useState([]);
+  const [searchbox,setSearchbox] = useState("");
+
+  console.log("APPP")
+
+          useEffect(()=>{
+              fetch('https://jsonplaceholder.typicode.com/users')
+              .then(response => response.json())
+              .then(users => {
+              setRobots(users)
+              }
+              )
+              console.log("useeffecttt")
+         
+          },[])
+              
+
+           
+          
+          const onSearchChange = (event)=>{  
+              setSearchbox(event.target.value)
+              console.log(searchbox)
+          };
+          const filteredRobots =  robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchbox.toLowerCase())
+            
+        }
+        )
+      
+
+  return !robots.length ? <h1>loading robots </h1> :
+  (
+    <>
+      <Searchbox onfilter={onSearchChange}/>
+        <Scroll>
+        <CardList robots = {filteredRobots}/>
+        </Scroll>
+      
+      {console.log("body")}
+    </>
+  )
+}
+
+export default App
